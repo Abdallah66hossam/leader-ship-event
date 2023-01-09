2b1b5e1d0f01791c4a521263bbf6243a3bb220e0
abdallah <bedohossam535@gmail.com>
2023-01-09 23:18:15 +0200

    finished responsive of speakers section

--- src/components/speakers/Speakers.js
+++ src/components/speakers/Speakers.js
@@ -41,94 +41,98 @@ const Speakers = () => {
         </div>
       </div>
       <div className={style.people}>
-        <div
-          className={style.human}
-          style={{ backgroundImage: `url(${img1})` }}
-        >
-          <div className={style.text}>
-            <div className={style.title}>
-              <h3>Natalie</h3>
-              <h4>Event Planner</h4>
-            </div>
-            <div className={style.links}>
-              <div className={style.icon}>
-                <BsFacebook />
+        <section>
+          <div
+            className={style.human}
+            style={{ backgroundImage: `url(${img1})` }}
+          >
+            <div className={style.text}>
+              <div className={style.title}>
+                <h3>Natalie</h3>
+                <h4>Event Planner</h4>
               </div>
-              <div className={style.icon}>
-                <BsInstagram />
-              </div>
-              <div className={style.icon}>
-                <BsWhatsapp />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className={style.human}
-          style={{ backgroundImage: `url(${img3})` }}
-        >
-          <div className={style.text}>
-            <div className={style.title}>
-              <h3>Thomas</h3>
-              <h4>STARTUP COACH</h4>
-            </div>
-            <div className={style.links}>
-              <div className={style.icon}>
-                <BsFacebook />
-              </div>
-              <div className={style.icon}>
-                <BsInstagram />
-              </div>
-              <div className={style.icon}>
-                <BsWhatsapp />
+              <div className={style.links}>
+                <div className={style.icon}>
+                  <BsFacebook />
+                </div>
+                <div className={style.icon}>
+                  <BsInstagram />
+                </div>
+                <div className={style.icon}>
+                  <BsWhatsapp />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div
-          className={style.human}
-          style={{ backgroundImage: `url(${img4})` }}
-        >
-          <div className={style.text}>
-            <div className={style.title}>
-              <h3>Isabella</h3>
-              <h4>Event Manager</h4>
-            </div>
-            <div className={style.links}>
-              <div className={style.icon}>
-                <BsFacebook />
+          <div
+            className={style.human}
+            style={{ backgroundImage: `url(${img3})` }}
+          >
+            <div className={style.text}>
+              <div className={style.title}>
+                <h3>Thomas</h3>
+                <h4>STARTUP COACH</h4>
               </div>
-              <div className={style.icon}>
-                <BsInstagram />
-              </div>
-              <div className={style.icon}>
-                <BsWhatsapp />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className={style.human}
-          style={{ backgroundImage: `url(${img5})` }}
-        >
-          <div className={style.text}>
-            <div className={style.title}>
-              <h3>Samantha</h3>
-              <h4>Top Level Speaker</h4>
-            </div>
-            <div className={style.links}>
-              <div className={style.icon}>
-                <BsFacebook />
-              </div>
-              <div className={style.icon}>
-                <BsInstagram />
-              </div>
-              <div className={style.icon}>
-                <BsWhatsapp />
+              <div className={style.links}>
+                <div className={style.icon}>
+                  <BsFacebook />
+                </div>
+                <div className={style.icon}>
+                  <BsInstagram />
+                </div>
+                <div className={style.icon}>
+                  <BsWhatsapp />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
+        <section>
+          <div
+            className={style.human}
+            style={{ backgroundImage: `url(${img4})` }}
+          >
+            <div className={style.text}>
+              <div className={style.title}>
+                <h3>Isabella</h3>
+                <h4>Event Manager</h4>
+              </div>
+              <div className={style.links}>
+                <div className={style.icon}>
+                  <BsFacebook />
+                </div>
+                <div className={style.icon}>
+                  <BsInstagram />
+                </div>
+                <div className={style.icon}>
+                  <BsWhatsapp />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className={style.human}
+            style={{ backgroundImage: `url(${img5})` }}
+          >
+            <div className={style.text}>
+              <div className={style.title}>
+                <h3>Samantha</h3>
+                <h4>Top Level Speaker</h4>
+              </div>
+              <div className={style.links}>
+                <div className={style.icon}>
+                  <BsFacebook />
+                </div>
+                <div className={style.icon}>
+                  <BsInstagram />
+                </div>
+                <div className={style.icon}>
+                  <BsWhatsapp />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
