12aedb8835f176e1fd6568f3ce36d539eefe6457
abdallah <bedohossam535@gmail.com>
2023-01-09 20:56:05 +0200

    finishing nav bar responsive

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import style from "./nav.module.scss";
 import { BiBullseye } from "react-icons/bi";
+import { HiBars3 } from "react-icons/hi2";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const NavBar = () => {
   const [scroll, setScroll] = useState(false);
+  const [nav, setNav] = useState(false);
   const handelScrollNav = () => {
     if (window.scrollY > 100) {
       setScroll(true);
@@ -18,6 +21,14 @@ const NavBar = () => {
 
   return (
     <nav className={scroll ? style.back : ""}>
+      <HiBars3
+        className={!nav ? style.bars : `${style.opacity} ${style.bars}`}
+        onClick={() => setNav(!nav)}
+      />
+      <AiOutlineCloseCircle
+        className={nav ? style.close : `${style.opacity} ${style.close}`}
+        onClick={() => setNav(!nav)}
+      />
       <a href="home" className={style.logo}>
         <BiBullseye className={style.bullseye} />
         <span className={style.brand}>
@@ -25,41 +36,53 @@ const NavBar = () => {
         </span>
       </a>
 
-      <ul className={style.links}>
+      <ul className={!nav ? style.links : `${style.opacity} ${style.links}`}>
         <li>
-          <a href="#Home">Home</a>
+          <a href="#Home" onClick={() => setNav(!nav)}>
+            Home
+          </a>
         </li>
 
         <li>
-          <a href="#About">About</a>
+          <a href="#About" onClick={() => setNav(!nav)}>
+            About
+          </a>
         </li>
 
         <li>
-          <a href="#Speakers">Speakers</a>
+          <a href="#Speakers" onClick={() => setNav(!nav)}>
+            Speakers
+          </a>
         </li>
 
         <li>
-          <a href="#Schedules">Schedules</a>
+          <a href="#Schedules" onClick={() => setNav(!nav)}>
+            Schedules
+          </a>
         </li>
 
         <li>
-          <a href="#Pricing">Pricing</a>
+          <a href="#Pricing" onClick={() => setNav(!nav)}>
+            Pricing
+          </a>
         </li>
 
         <li>
-          <a href="#Venue">Venue</a>
+          <a href="#Venue" onClick={() => setNav(!nav)}>
+            Venue
+          </a>
         </li>
 
         <li>
-          <a href="#Contact">Contact</a>
-        </li>
-
-        <li>
-          <button>
-            <a href="#Buy-Tickets">Buy Tickets</a>
-          </button>
+          <a href="#Contact" onClick={() => setNav(!nav)}>
+            Contact
+          </a>
         </li>
       </ul>
+
+      <button className={style.tickets}>
+        <a href="#Buy-Tickets">Buy Tickets</a>
+      </button>
     </nav>
   );
 };
